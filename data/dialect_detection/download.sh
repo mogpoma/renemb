f3bcdbccab221b0284d819516b30b76991ffdc8c
mogpoma <139449837+mogpoma@users.noreply.github.com>
2023-09-29 10:54:28 +0200

    Dialect detection data

---
+++ data/dialect_detection/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+megadl 'https://mega.nz/file/pFtxzSSL#1zk2D0i9vIxi8gLyp_2FwB_VDW6rLn2GWC1DBneKt_g'
+megadl 'https://mega.nz/file/VREhwaSI#eNFtQ09JhU9xLmKkr_EtPW0VqPH8YH1V9BpjPUgVoM8'
+megadl 'https://mega.nz/file/0Q1hDQoQ#-5NuDvq8iHZS29kqMwldGQ5qv-eWDNlSDz2LBfkVOb4'
--- data/dialect_detection/extract.sh
+++ data/dialect_detection/extract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 tar -xvzf ./train_augmented.tar.gz
 tar -xvzf ./dev_augmented.tar.gz
-tar -xvzf ./test.tar.gz
-tar -xvzf ./difficult.tar.gz
+tar -xvzf ./test_augmented.tar.gz
+
